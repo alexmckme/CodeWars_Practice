5ab72d282a6250eb6f2a6beee0ba909bcae63d78
alexmckme <mlexka@yahoo.fr>
2023-11-27 21:46:39 +0100

    added new challenge

---
+++ 6kyu/Split Strings.js
@@ -0,0 +1,19 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//
+//     Examples:
+//
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+    if (str.length % 2 === 1) {
+        str += "_"
+    }
+    let finalArr = []
+
+    for (let i = 0; i < str.length; i=i+2) {
+        finalArr.push(str.slice(i, i+2))
+    }
+
+    return finalArr
+}
